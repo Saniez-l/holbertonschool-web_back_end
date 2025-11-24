966ba2f15033d2ffe530e09376b68d30c27395ed
Saniez-l <saniezl.christophe@gmail.com>
2025-11-24 22:27:39 +0100

    fix pycodestyle and using typing list

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -6,6 +6,6 @@ values and return sum with float value
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Return the sum of a list of mixed list."""
     return sum(mxd_lst)
