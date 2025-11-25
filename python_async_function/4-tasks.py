bd2b3eb96bd4cf69e13812860423735e96e82fec
Saniez-l <saniezl.christophe@gmail.com>
2025-11-25 22:09:02 +0100

    changing list with List

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -4,10 +4,11 @@ Execute multiple tasks created by task_wait_random
 """
 
 import asyncio
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Run task_wait_random n times and return delays in ascending order."""
 
     tasks = []
@@ -22,4 +23,4 @@ async def task_wait_n(n: int, max_delay: int) -> list[float]:
         delay = await task
         delays.append(delay)
 
-    return sorted(delays)
+    return delays
