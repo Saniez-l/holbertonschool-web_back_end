57cff63e21dbf0175d2d54840a5ca290a8bd5a66
Saniez-l <saniezl.christophe@gmail.com>
2025-12-02 22:48:00 +0100

    add {s] at word topic

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -6,7 +6,7 @@ school having a specific topic
 """
 
 
-def schools_by_topic(mongo_collection, topic):
-    cursor = mongo_collection.find({"topic": topic})
+def schools_by_topic(mongo_collection, topics):
+    cursor = mongo_collection.find({"topics": topics})
     document = list(cursor)
     return document
