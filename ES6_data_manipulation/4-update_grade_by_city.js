7cfbe3676f69a7a703afdb54860c93e48c229f0b
Saniez-l <saniezl.christophe@gmail.com>
2025-12-15 17:09:12 +0100

    add file 1, 2, 3, 4, 5, 6, 7, 8, 9

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.filter((grade) => grade.studentId === student.id);
+      const grade = gradeObj.length > 0 ? gradeObj[0].grade : 'N/A';
+      return {
+        ...student,
+        grade,
+      };
+    });
+}
